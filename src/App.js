b9d45c8cb9ed9628caee3f3b322f280675e28345
AdilMahmoudRion <adilmahmoudrion@gmail.com>
2021-12-01 03:47:02 +0600

    banner section created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from 'react';
+import './App.css';
 import Banner from "./Components/Pages/Banner/Banner";
 import Navigation from "./Components/Pages/Navigation/Navigation";
 
 function App() {
   return (
-    <div >
+    <div className="adilrionWebBody">
       <Navigation></Navigation>
       <Banner></Banner>
     </div>
--- src/Components/Pages/Banner/Banner.js
+++ src/Components/Pages/Banner/Banner.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import './Banner.css';
 
 const Banner = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+	return (
+		<div className="banner-section">
+			<div className="banner-item">
+				<div className="banner-img-section">
+					<img className="banner-img" src="https://cdn.pixabay.com/photo/2021/08/04/13/06/software-developer-6521720__480.jpg" alt="" srcset="" />
+				</div>
+				<div className="banner-information">
+					<p className="banner-skill">Web Developer</p>
+					<h1>Adil M Rion</h1>
+					<p>Fusce tempor magna mi, non egestas velit ultricies nec. Aenean convallis, risus non condimentum gravida, odio mauris ullamcorper felis, ut venenatis purus ex eu mi. Quisque imperdiet lacinia urna, a placerat sapien pretium eu.</p>
+					<div>
+						<button className="banner-btn1 btn">Download CV</button>
+						<button className="banner-btn2 btn">Contact</button>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+	);
 };
 
 export default Banner;
