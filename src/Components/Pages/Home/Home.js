c4ec6789cf8b335af089a7dc8c824cec6f7ba59a
AdilMahmoudRion <adilmahmoudrion@gmail.com>
2021-12-01 06:47:02 +0600

    service section addded

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navigation from '../Navigation/Navigation'
 import Banner from '../Banner/Banner'
 import About from '../About/About';
+import Service from '../Service/Service';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
            <Navigation></Navigation>
            <Banner></Banner>
            <About></About>
+           <Service></Service>
         </div>
     );
 };
---
+++ src/Components/Pages/Service/Service.js
@@ -0,0 +1,108 @@
+import React from "react";
+import './Service.css';
+const Service = () => {
+  return (
+    <div>
+      <div className="service">
+        <div className="service-header">
+          <p className="hed">WHAT WE DO</p>
+          <h1 className="service-header">SERVICES AND SOLUTIONS</h1>
+        </div>
+        <div className="Service-section">
+          <div className="service-list">
+            <div>
+              <img className="service-img"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8YN6c1eYF2UHcOt3x6xyubxJkmwdMCsREgDIn5CcxP2mkgf-xQsQVQBaYYlelEBWwcyk&usqp=CAU"
+                alt=""
+              />
+            </div>
+            <div className="service-text">
+              <h2>CREATIVE DESIGN</h2>
+              <p className="service-text-details">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum indust.
+              </p>
+            </div>
+          </div>
+          <div className="service-list">
+            <div>
+              <img className="service-img"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8YN6c1eYF2UHcOt3x6xyubxJkmwdMCsREgDIn5CcxP2mkgf-xQsQVQBaYYlelEBWwcyk&usqp=CAU"
+                alt=""
+              />
+            </div>
+            <div className="service-text">
+              <h2>CREATIVE DESIGN</h2>
+              <p className="service-text-details">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum indust.
+              </p>
+            </div>
+          </div>
+          <div className="service-list">
+            <div>
+              <img className="service-img"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8YN6c1eYF2UHcOt3x6xyubxJkmwdMCsREgDIn5CcxP2mkgf-xQsQVQBaYYlelEBWwcyk&usqp=CAU"
+                alt=""
+              />
+            </div>
+            <div className="service-text">
+              <h2>CREATIVE DESIGN</h2>
+              <p className="service-text-details">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum indust.
+              </p>
+            </div>
+          </div>
+          <div className="service-list">
+            <div>
+              <img className="service-img"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8YN6c1eYF2UHcOt3x6xyubxJkmwdMCsREgDIn5CcxP2mkgf-xQsQVQBaYYlelEBWwcyk&usqp=CAU"
+                alt=""
+              />
+            </div>
+            <div className="service-text">
+              <h2>CREATIVE DESIGN</h2>
+              <p className="service-text-details">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum indust.
+              </p>
+            </div>
+          </div>
+          <div className="service-list">
+            <div>
+              <img className="service-img"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8YN6c1eYF2UHcOt3x6xyubxJkmwdMCsREgDIn5CcxP2mkgf-xQsQVQBaYYlelEBWwcyk&usqp=CAU"
+                alt=""
+              />
+            </div>
+            <div className="service-text">
+              <h2>CREATIVE DESIGN</h2>
+              <p className="service-text-details">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum indust.
+              </p>
+            </div>
+          </div>
+          <div className="service-list">
+            <div>
+              <img className="service-img"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8YN6c1eYF2UHcOt3x6xyubxJkmwdMCsREgDIn5CcxP2mkgf-xQsQVQBaYYlelEBWwcyk&usqp=CAU"
+                alt=""
+              />
+            </div>
+            <div className="service-text">
+              <h2>CREATIVE DESIGN</h2>
+              <p className="service-text-details">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum indust.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Service;
