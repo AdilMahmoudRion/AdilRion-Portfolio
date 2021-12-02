85d53f48458a63bf075e4257d00f4bc270ac5cbc
AdilMahmoudRion <adilmahmoudrion@gmail.com>
2021-12-02 00:23:49 -1200

    contact from style added

--- src/Components/Pages/Contact/Contact.js
+++ src/Components/Pages/Contact/Contact.js
@@ -1,61 +1,111 @@
-import { faEnvelope, faMapMarkedAlt, faPhoneVolume } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import emailjs from 'emailjs-com';
-import './Contact.css';
+import {
+  faEnvelope,
+  faMapMarkedAlt,
+  faPhoneVolume,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import emailjs from "emailjs-com";
+import "./Contact.css";
 const Contact = () => {
+  function sendEmail(e) {
+    e.preventDefault();
 
-    function sendEmail(e) {
-        e.preventDefault();
-
-    emailjs.sendForm('service_7ff0j7m', 'my_portfolio', e.target, 'user_d6UB7RpQSTmAoLqZ1Mu3c')
-        .then((result) => {
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
-        });
-        e.target.reset()
-    }
-  
+    emailjs
+      .sendForm(
+        "service_7ff0j7m",
+        "my_portfolio",
+        e.target,
+        "user_d6UB7RpQSTmAoLqZ1Mu3c"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  }
 
   return (
     <div className="contact-from">
-      <div>
-            <h2 className="header">INFORMATION</h2>
-            <h1>CONTACT INFORMATION</h1>
-            <p>
-            Event definition is - somthing that happens occurre How evesnt
-            sentence. Synonym when an unknown printer took a galley.
+      <div className="add-info">
+        <h2 className="header">INFORMATION</h2>
+        <h1>CONTACT INFORMATION</h1>
+        <p>
+          Event definition is - somthing that happens occurre How evesnt
+          sentence. Synonym when an unknown printer took a galley.
+        </p>
+        <h4>
+          OFFICE IN <strong>DHAKA</strong>
+        </h4>
+        <div className="address">
+          <div className="address-detail">
+            <p className="icon">
+              <FontAwesomeIcon icon={faMapMarkedAlt} />
+            </p>{" "}
+            <p>ADDRESS: Dhaka, Bangladesh</p>
+          </div>
+          <div className="address-detail">
+            <p className="icon">
+              <FontAwesomeIcon icon={faPhoneVolume} />
             </p>
-            <h4>OFFICE IN <strong>DHAKA</strong></h4>
-            <div className="address">
-            <div className="address-detail"><p><FontAwesomeIcon icon={faMapMarkedAlt} /></p> <p>ADDRESS: Dhaka, Bangladesh</p></div>
-            <div className="address-detail"><p><FontAwesomeIcon icon={faPhoneVolume} /></p><p>PHONE: +8801833449963</p></div>
-            <div className="address-detail"><p><FontAwesomeIcon icon={faPhoneVolume} /></p><p>PHONE: +8801722234443</p></div>
-            <div className="address-detail"><p><FontAwesomeIcon icon={faEnvelope} /></p><p>EMAIL: adilmahmoudrion@gmail.com</p></div>
-            </div>
+            <p>PHONE: +8801833449963</p>
+          </div>
+          <div className="address-detail">
+            <p className="icon">
+              <FontAwesomeIcon icon={faPhoneVolume} />
+            </p>
+            <p>PHONE: +8801722234443</p>
+          </div>
+          <div className="address-detail">
+            <p className="icon">
+              <FontAwesomeIcon icon={faEnvelope} />
+            </p>
+            <p>EMAIL: adilmahmoudrion@gmail.com</p>
+          </div>
+        </div>
       </div>
       <div>
-      <div className="container">
-            <form onSubmit={sendEmail}>
-                    <div className="row pt-5 mx-auto">
-                        <div className="col-8 form-group mx-auto">
-                            <input type="text" className="form-control" placeholder="Name" name="name"/>
-                        </div>
-                        <div className="col-8 form-group pt-2 mx-auto">
-                            <input type="email" className="form-control" placeholder="Email Address" name="email"/>
-                        </div>
-                        <div className="col-8 form-group pt-2 mx-auto">
-                            <input type="text" className="form-control" placeholder="Subject" name="subject"/>
-                        </div>
-                        <div className="col-8 form-group pt-2 mx-auto">
-                            <textarea className="form-control" id="" cols="30" rows="8" placeholder="Your message" name="message"></textarea>
-                        </div>
-                        <div className="col-8 pt-3 mx-auto">
-                            <input type="submit" className="btn btn-info" value="Send Message"></input>
-                        </div>
-                    </div>
-                </form>
+        <div className="form-section">
+          <form onSubmit={sendEmail}>
+            <div className="form">
+              <input
+                type="text"
+                className="form-control inputFrom"
+                placeholder="Name"
+                name="name"
+              />
+
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Email Address"
+                name="email"
+              />
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Subject"
+                name="subject"
+              />
+              <textarea
+                className="form-control"
+                id=""
+                cols="30"
+                rows="6"
+                placeholder="Your message"
+                name="message"
+              ></textarea>
+              <input
+                type="submit"
+                className="from-btn"
+                value="SEND MESSAGE"
+              ></input>
             </div>
+          </form>
+        </div>
       </div>
     </div>
   );
